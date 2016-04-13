8157963789ac7d829e178dca37331a92b9cc8662
error25 <autovincelynch@gmail.com>
2016-04-13 19:31:35 +0100

    heroku check

--- config/app.js
+++ config/app.js
@@ -2,5 +2,5 @@ module.exports = {
   port: process.env.PORT || 8000,
   databaseUrl: 'mongodb://heroku_clxsvx42:m54mdijfr0lcgh1qronm5094sc@ds023550.mlab.com:23550/heroku_clxsvx42',
   secret: 'notVerySecret',
-  appUrl: 'http://localhost:8000'
+  appUrl: 'https://desolate-dawn-81846.herokuapp.com'
 }
