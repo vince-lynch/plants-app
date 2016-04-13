3080f32f27b98e290ee797298cd0439297518f92
error25 <autovincelynch@gmail.com>
2016-04-13 18:39:02 +0100

    fixing cors issue

--- app.js
+++ app.js
@@ -29,11 +29,16 @@ app.use(express.static(__dirname + '/views'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({
+/*app.use(cors({
   origin: config.appUrl,
   credentials: true
-}));
+}));*/
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function(req, res) {
   res.render('index');
