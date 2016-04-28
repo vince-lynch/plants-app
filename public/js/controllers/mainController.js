61dc5a15269990f9be4ec91addf398f13e732ed9
error25 <autovincelynch@gmail.com>
2016-04-28 11:29:39 +0100

    checking the CORS works, then I#ll do backend login

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -17,7 +17,73 @@ function MainController($auth, tokenService, $resource, $window, $state, GEOCODE
   self.seg10 = true;
   self.seg11 = true;*/
   self.wateringCount = 0;
+  self.currentweatherstate = "";
+  self.currentweathercode = 0;
+  self.currentweathertemp = 0;
+
   $scope.sunny = true;
+
+
+  self.changeweathergraphic = function(){
+
+    console.log("current weather code is " + self.currentweathercode)
+    self.currentweathercode = parseInt(self.currentweathercode)
+    console.log(self.currentweathercode);
+    // clear weather state
+    $scope.sunny = false;
+    $scope.cloudy = false;
+    $scope.rainy = false;
+    $scope.snowy = false;
+    $scope.rainbow = false;
+    $scope.starry = false;
+    $scope.stormy = false;
+
+    var currentweathercode = self.currentweathercode;
+
+    if (currentweathercode == 3 || currentweathercode == 4 ||currentweathercode ==  37 ||  currentweathercode ==  38 || currentweathercode ==  39 ||currentweathercode ==  45 || currentweathercode == 47 ){
+      console.log("stormy if condition met");
+      $scope.stormy = true;
+    } else if (currentweathercode == 5 ||currentweathercode == 6 ||currentweathercode == 7 ||currentweathercode == 8 ||currentweathercode == 10 ||currentweathercode == 13 ||currentweathercode == 14 ||currentweathercode == 15 ||currentweathercode == 16 ||currentweathercode == 17 ||currentweathercode == 18 ||currentweathercode == 46 ||currentweathercode == 41 ||currentweathercode == 42 ||currentweathercode == 43 ||currentweathercode == 35){
+      $scope.snowy = true;
+    } else if (currentweathercode == 11 ||currentweathercode == 12 ||currentweathercode == 40){
+      $scope.rainy = true;
+    } else if(currentweathercode == 19 || currentweathercode == 20 || currentweathercode == 22 || currentweathercode == 23 || currentweathercode == 24 || currentweathercode == 26 || currentweathercode == 28 || currentweathercode == 29 || currentweathercode == 30){
+      $scope.cloudy = true;
+    } else if(currentweathercode == 27 ||currentweathercode == 29 ||currentweathercode == 31 ||currentweathercode == 33){
+      $scope.starry = true;
+    } else if(currentweathercode == 27 ||currentweathercode == 29 ||currentweathercode == 31 ||currentweathercode == 33){
+      $scope.starry = true;
+    } else if(currentweathercode == 32 ||currentweathercode == 34 ||currentweathercode == 36){
+      $scope.sunny = true;
+    }
+  }
+
+  self.getWeather = function() {
+    $http({
+      method: 'GET',
+      url: 'https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast ' +
+            'where woeid in (select woeid from geo.places(1) where text="London")&format=json'
+    }).then(function successCallback(response) {
+        console.log(response);
+        console.log("The temperatute in London is " + response.data.query.results.channel.item.condition.temp + response.data.query.results.channel.item.condition.text);
+
+        self.currentweatherstate = response.data.query.results.channel.item.condition.text;
+
+        self.currentweathercode = response.data.query.results.channel.item.condition.code;
+
+        var temp = parseInt(response.data.query.results.channel.item.condition.temp);
+
+        self.currentweathertemp = (5/9) * (temp-32);
+       
+
+        self.changeweathergraphic()
+
+      }, function errorCallback(response) {
+        console.log(response);
+      });
+  }
+
+  self.getWeather();
 
   $scope.growplant = function(){
     console.log("function called")
